10b17f3757f92f086648c670231a40bf455b7c9f
DanielTruong999 <datthanh01237344449@gmail.com>
2024-08-12 02:01:53 +0900

    Add test script

---
+++ test.py
@@ -0,0 +1,36 @@
+import torch 
+from learning import Learner
+from train_cfg import IrisTrainCfg
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+import os 
+from learning.dataset import CSVDataset
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+
+train_cfg = IrisTrainCfg()
+learner = Learner(train_cfg)
+
+test_dataset = CSVDataset(os.path.join(train_cfg.dataset.dataset_path, 'test'))
+learner.data_loaders['test'] = DataLoader(
+    test_dataset, 
+    batch_size=len(test_dataset), 
+    shuffle=False
+)
+
+
+learner.model.load_state_dict(torch.load('logs/iris_classifier_20240812_012235/model_20240812_012235_713', map_location=learner.device))
+
+with torch.no_grad():
+    learner.model.eval()
+    for index, data in enumerate(learner.data_loaders['test']):
+        inputs, labels = data['input'].to(learner.device), data['label'].to(learner.device)
+        predicts = learner.model(inputs)
+        predicts = (predicts >= 0.5).long()
+
+        cm = confusion_matrix(labels.cpu().numpy(), predicts.cpu().numpy())
+
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=[0, 1])
+disp.plot(cmap=plt.cm.Blues)
+plt.title('Confusion Matrix')
+plt.show()
+        
