ab9b94d5cafcfd3acd72902520b59e816931bd7e
DanielTruong999 <datthanh01237344449@gmail.com>
2024-08-13 16:08:15 +0900

    Add measure execution time

--- test.py
+++ test.py
@@ -6,25 +6,29 @@ import os
 from learning.dataset import CSVDataset
 from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
+import time 
 
 train_cfg = IrisTrainCfg()
 learner = Learner(train_cfg)
-
 test_dataset = CSVDataset(os.path.join(train_cfg.dataset.dataset_path, 'test'))
 learner.data_loaders['test'] = DataLoader(
     test_dataset, 
     batch_size=len(test_dataset), 
     shuffle=False
 )
-
-
 learner.model.load_state_dict(torch.load('logs/iris_classifier_20240812_012235/model_20240812_012235_713', map_location=learner.device))
 
 with torch.no_grad():
     learner.model.eval()
     for index, data in enumerate(learner.data_loaders['test']):
         inputs, labels = data['input'].to(learner.device), data['label'].to(learner.device)
+        
+        start_time = time.time()
         predicts = learner.model(inputs)
+        end_time = time.time()
+        
+        print(f"Time taken for computation: {((end_time - start_time)/len(test_dataset))/1e-3} milliseconds")
+
         predicts = (predicts >= 0.5).long()
 
         cm = confusion_matrix(labels.cpu().numpy(), predicts.cpu().numpy())
